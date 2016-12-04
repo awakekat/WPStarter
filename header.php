efd3d7e52342afb6104f366430d0da27839336ca
Katherine <awakekat@aol.com>
2016-12-03 23:14:14 -0500

    add wp to header

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- commented out change font name and size <link href='https://fonts.googleapis.com/css?family=Mr+Bedfort|Alegreya:400,700' rel='stylesheet' type='text/css'> -->
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
 </head>
-<body>
-h1  
-
+<body <?php body_class(); ?>>
+  <div class="header">
+  </div>
