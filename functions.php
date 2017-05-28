29bf0e2cbd7043a4eb6a7c2f3341c0aab316a691
Katherine Wakefields <awakekat@aol.com>
2017-05-27 23:18:31 -0400

    add bootstrap font awesome jquery edit functions

--- functions.php
+++ functions.php
@@ -20,21 +20,16 @@ add_filter( 'script_loader_src', 'awakekat_remove_wp_version' );
 add_filter( 'style_loader_src', 'awakekat_remove_wp_version' );
 
 
-/* ===================================
- *  CSS and Javascript - Bower install
- * ===================================  */ 
+/* ======================================
+ *  CSS and Javascript - Bower Components
+ * ==================================== */
 function sitename_styles() {
 
-  wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Mr+Bedfort|Alegreya:400,700', false);
+  wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Mr+Bedfort|Alegreya:400,700');
 
   wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css');
 
   wp_enqueue_style('awesome-style', get_template_directory_uri() . '/css/font-awesome.min.css');
-
-  wp_enqueue_style('devicons-style', get_template_directory_uri() . '/css/devicons.min.css');
-
-  wp_enqueue_script('mixitupjs', get_template_directory_uri() . '/js/jquery.mixitup.min.js',
-         array('jquery'), '', false);
 
   wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js',
          array('jquery'), '', true);
@@ -49,36 +44,23 @@ add_action('wp_enqueue_scripts', 'sitename_styles');
 
 /* ============
  *  Navigation
- * ===========  */ 
+ * ===========  */
 function sitename_register_menu() {
   register_nav_menu( 'primary',__( 'Primary Menu', 'awakekat' ) );
 }
 add_action( 'init', 'sitename_register_menu' );
 
 
-/* ===================
- * Limit Post Title 
- * ===================  */ 
-function max_post_title_length($title) {
-  global $post;
-  $title = $post->post_title;
-  if (str_word_count($title) >= 4)
-    wp_die( __('Error: your post title is over the maximum 3 word count.'));
-}
-add_action('publish_post', 'max_post_title_length');
-
-
 /* =======================
  * Change Footer in Admin
- * =====================*/ 
+ * =====================*/
 function sitename_footer_admin () {
   echo 'Theme designed and Developed by <a href="http://awakekat.com" "target=_blank">Awakekat</a> and powered by <a href="http://wordpress.org" "target=_blank">Wordpress</a>.';
 }
 add_filter('admin_footer_text', 'sitename_footer_admin');
 
 
-/* ================= 
+/* ==================
  * Add Featured Image
  * ================== */
 add_theme_support( 'post-thumbnails' );
-
